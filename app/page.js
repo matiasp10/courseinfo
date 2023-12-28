0c64af8b4e0a1912aab653633df75e80a0cc17f7
Matias Petenatti <matiasodin96@gmail.com>
2023-12-28 19:20:21 -0300

    2.1

--- app/page.js
+++ app/page.js
@@ -1,31 +1,52 @@
 import Content from '@/components/Content';
+import Course from '@/components/Course';
 import Header from '@/components/Header';
 import Total from '@/components/Total';
 
 export default function Home() {
+  // const course = {
+  //   name: 'Half Stack application development',
+  //   parts: [
+  //     {
+  //       name: 'Fundamentals of React',
+  //       exercises: 10,
+  //     },
+  //     {
+  //       name: 'Using props to pass data',
+  //       exercises: 7,
+  //     },
+  //     {
+  //       name: 'State of a component',
+  //       exercises: 14,
+  //     },
+  //   ],
+  // };
+
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10,
+        id: 1,
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
+        id: 2,
       },
       {
         name: 'State of a component',
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={course.parts} />
+      <Course course={course} />
     </div>
   );
 }
